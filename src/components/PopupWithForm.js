9b305f4b982f6027bfb6971850e44fac26a6915a
Sergey Dedikov <serded59@yandex.ru>
2021-11-12 21:35:15 +0500

    create component PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,28 @@
+function PopupWithForm({ name, title, children }) {
+  return (
+    <div className={`popup popup_type_${props.name}`}>
+      <div className={`popup__container popup__container_type_${props.name}`}>
+        <form
+          name={`${props.name}`}
+          className={`popup__form popup__form_${props.name}`}
+          noValidate
+        >
+          <h3 className="popup__heading">{props.title}</h3>
+          {props.children}
+          <button
+            className="popup__button button"
+            type="submit"
+          >
+            {props.name} // временно
+          </button>
+          <button
+            className="popup__close button"
+            type="button"
+          ></button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
