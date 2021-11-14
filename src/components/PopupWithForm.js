e3a3496941fa3af53a9a0d78fbe8f993ccb92796
Sergey Dedikov <serded59@yandex.ru>
2021-11-14 21:32:51 +0500

    feat: refactor PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,21 +1,11 @@
 function PopupWithForm(props) {
-
-  const popup = document
-  .querySelector(`.popup_type_${props.name}`);
-
-  function openPopup() {
-    popup.classList.toggle("popup_opened", true);
-  }
-
-  function closePopup() {
-    //popup.classList.toggle("popup_opened", false);
-  }
-
-  props.isOpen ? openPopup() : closePopup();
-  
-  
   return (
-    <div className={`popup popup_type_${props.name}`}>
+    <div
+      className={`
+        popup popup_type_${props.name}
+        ${props.isOpen ? "popup_opened" : ""}
+      `}
+    >
       <div className={`popup__container popup__container_type_${props.name}`}>
         <form
           name={`${props.name}`}
