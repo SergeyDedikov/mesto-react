d6882217cd4e30ac682b18f8058e0b9df7f41c5a
Sergey Dedikov <serded59@yandex.ru>
2021-11-15 17:40:46 +0500

    feat: add Data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 function Footer() {
+
+  const data = new Date();
+  
   return (
     <footer className="footer">
       <p className="footer__copyright" lang="en">
-        &copy; 2021 Mesto Russia & Sergey Dedikov
+        &copy; {data.getFullYear()} Mesto Russia & Sergey Dedikov
       </p>
     </footer>
   );
