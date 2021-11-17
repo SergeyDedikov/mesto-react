1b84f7598abde987b3e0c52a8c13af31467c6ca6
Sergey Dedikov <serded59@yandex.ru>
2021-11-17 14:33:27 +0500

    bugfix: refactor children in Popupwithform

--- src/components/App.js
+++ src/components/App.js
@@ -56,99 +56,95 @@ function App() {
         name={"edit-avatar"}
         title={"Обновить аватар"}
         textButtonSubmit={"Сохранить"}
-        children={
-          <fieldset className="popup__input-container">
-            <label className="popup__field">
-              <input
-                id="avatar"
-                className="popup__input popup__input_value_link"
-                type="url"
-                name="avatar"
-                placeholder="Ссылка на аватар"
-                required
-              />
-              <span id="avatar-error" className="popup__error"></span>
-            </label>
-          </fieldset>
-        }
-      />
+      >
+        <fieldset className="popup__input-container">
+          <label className="popup__field">
+            <input
+              id="avatar"
+              className="popup__input popup__input_value_link"
+              type="url"
+              name="avatar"
+              placeholder="Ссылка на аватар"
+              required
+            />
+            <span id="avatar-error" className="popup__error"></span>
+          </label>
+        </fieldset>
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isEditProfilePopupOpen}
         onClose={closeAllPopups}
         name={"edit-profile"}
         title={"Редактировать профиль"}
         textButtonSubmit={"Сохранить"}
-        children={
-          <fieldset className="popup__input-container">
-            <label className="popup__field">
-              <input
-                id="name"
-                className="popup__input popup__input_value_name"
-                type="text"
-                name="name"
-                required
-                minLength="2"
-                maxLength="40"
-              />
-              <span id="name-error" className="popup__error"></span>
-            </label>
-            <label className="popup__field">
-              <input
-                id="job"
-                className="popup__input popup__input_value_job"
-                type="text"
-                name="job"
-                required
-                minLength="2"
-                maxLength="200"
-              />
-              <span id="job-error" className="popup__error"></span>
-            </label>
-          </fieldset>
-        }
-      />
+      >
+        <fieldset className="popup__input-container">
+          <label className="popup__field">
+            <input
+              id="name"
+              className="popup__input popup__input_value_name"
+              type="text"
+              name="name"
+              required
+              minLength="2"
+              maxLength="40"
+            />
+            <span id="name-error" className="popup__error"></span>
+          </label>
+          <label className="popup__field">
+            <input
+              id="job"
+              className="popup__input popup__input_value_job"
+              type="text"
+              name="job"
+              required
+              minLength="2"
+              maxLength="200"
+            />
+            <span id="job-error" className="popup__error"></span>
+          </label>
+        </fieldset>
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isAddPlacePopupOpen}
         onClose={closeAllPopups}
         name={"add-place"}
         title={"Новое место"}
         textButtonSubmit={"Создать"}
-        children={
-          <fieldset className="popup__input-container">
-            <label className="popup__field">
-              <input
-                id="place"
-                className="popup__input popup__input_value_place"
-                type="text"
-                name="place"
-                placeholder="Название"
-                required
-                minLength="2"
-                maxLength="30"
-              />
-              <span id="place-error" className="popup__error"></span>
-            </label>
-            <label className="popup__field">
-              <input
-                id="link"
-                className="popup__input popup__input_value_link"
-                type="url"
-                name="link"
-                placeholder="Ссылка на картинку"
-                required
-              />
-              <span id="link-error" className="popup__error"></span>
-            </label>
-          </fieldset>
-        }
-      />
+      >
+        <fieldset className="popup__input-container">
+          <label className="popup__field">
+            <input
+              id="place"
+              className="popup__input popup__input_value_place"
+              type="text"
+              name="place"
+              placeholder="Название"
+              required
+              minLength="2"
+              maxLength="30"
+            />
+            <span id="place-error" className="popup__error"></span>
+          </label>
+          <label className="popup__field">
+            <input
+              id="link"
+              className="popup__input popup__input_value_link"
+              type="url"
+              name="link"
+              placeholder="Ссылка на картинку"
+              required
+            />
+            <span id="link-error" className="popup__error"></span>
+          </label>
+        </fieldset>
+      </PopupWithForm>
       <PopupWithForm
         isOpen={isConfirmationPopupOpen}
         onClose={closeAllPopups}
         name={"confirmation"}
         title={"Вы уверены?"}
         textButtonSubmit={"Да"}
-        children={null}
       />
       <ImagePopup card={selectedCard} onClose={closeAllPopups} />
     </>
